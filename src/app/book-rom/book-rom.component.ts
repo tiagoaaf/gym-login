a9ba572c143a1798c7409c38d3960504f7928a33
Tiago <tiagoandreaf95@gmail.com>
2019-07-04 14:19:09 +0100

    Book room structure and styling

--- src/app/book-rom/book-rom.component.ts
+++ src/app/book-rom/book-rom.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-rom',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookRomComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  username: string;
+  success: boolean;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.username = authService.getUser();
+    
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      room: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.success = true;
   }
 
 }
